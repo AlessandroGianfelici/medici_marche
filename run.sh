83887975377c85593ee08542476761044e6dcf9a
Alessandro Gianfelici <alessandro.gianfelici@hotmail.com>
2024-03-15 12:27:33 +0100

    first commit

---
+++ run.sh
@@ -0,0 +1,9 @@
+#brew install wget osrm-backend
+#wget https://download.geofabrik.de/europe/italy/centro-latest.osm.pbf --directory-prefix=osm_dumps
+
+osrm-extract -p osrm-backend/profiles/car.lua osm_dumps/centro-latest.osm.pbf
+osrm-partition osm_dumps/centro-latest.osrm
+osrm-customize osm_dumps/centro-latest.osrm
+osrm-contract osm_dumps/centro-latest.osrm 
+
+pip install -r requirements.txt
