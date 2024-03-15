253c7740193e626ed980987c340be230f221736c
Alessandro Gianfelici <alessandro.gianfelici@hotmail.com>
2024-03-15 13:38:42 +0100

    minor changes

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-#brew install wget osrm-backend
-#wget https://download.geofabrik.de/europe/italy/centro-latest.osm.pbf --directory-prefix=osm_dumps
+brew install wget osrm-backend
+wget https://download.geofabrik.de/europe/italy/centro-latest.osm.pbf --directory-prefix=osm_dumps
 
 osrm-extract -p osrm-backend/profiles/car.lua osm_dumps/centro-latest.osm.pbf
 osrm-partition osm_dumps/centro-latest.osrm
